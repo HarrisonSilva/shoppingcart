c6f68ef9a8c372c79e776769d9362ff88af0a7a7
Harrison <Harrison.couto@outlook.com>
2022-11-16 15:59:00 -0300

    testando se isFunction e uma funçao

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,15 +4,15 @@ import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
-  it('fetchProductsList é uma função', () => {
-    
+  it('fetchProductsList é uma função', async () => {
+    expect(fetchProductsList)
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
+  it('fetch é chamado ao executar fetchProductsList', async () => {
 
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
 
   });
 
