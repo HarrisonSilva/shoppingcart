74239d9c78fed4e247f20066574647b9486756b8
Harrison <Harrison.couto@outlook.com>
2022-11-17 18:15:29 -0300

    Implementando Loading na requisiçao da Api e implementando erro caso a api nao seja chamada

--- src/main.js
+++ src/main.js
@@ -6,11 +6,31 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const createLoading = () => {
+  const div = document.createElement('div');
+  div.innerHTML = 'carregando...';
+  div.className = 'loading';
+  document.querySelector('.products').appendChild(div);
+};
+
+const createError = () => {
+  const div = document.createElement('div');
+  div.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  div.className = 'error';
+  document.querySelector('.products').appendChild(div);
+};
+
 const viewProduct = async () => {
-  const arrayProduct = await fetchProductsList('computador');
-  arrayProduct.forEach((product) => {
-    const getItem = createProductElement(product);
-    document.querySelector('.products').appendChild(getItem);
-  });
+  createLoading();
+  try {
+    const arraDeProdutos = await fetchProductsList('computador');
+    arraDeProdutos.forEach((product) => {
+      const produto = createProductElement(product);
+      document.querySelector('.products').appendChild(produto);
+    });
+  } catch (error) {
+    createError();
+  }
+  document.querySelector('.loading').remove();
 };
 viewProduct();
